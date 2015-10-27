c1eae9ce9d0a4c43d413ac0c5960bd1147ddb559
unknown <watanabe.h@westside-PC.ad-axis.axissoft.co.jp>
2015-10-27 19:57:03 +0900

    leepmotion用

--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
 //LeepMotionTest　機材とSDKが必要。
 //プレゼンで使える。何も知らない上司の前で、フォースを操るようにスライドをめくる事により
-//なんとか内容を忘れてくれる必殺技。効果はばつぐん。
-//いろいろいじくってチャタリングも考慮しているので、
-//指の動きを二重に探知して、ネタバレ２重めくりの危険性も減っている。
+//なんとか内容を忘れてくれる必殺技が使えるようになる。効果は実証済み。
+//チャタリングも考慮しているので指の動きを二重に探知して、ネタバレ２重めくりの危険性も減っている。
 
 #include "stdafx.h"
 #include <Windows.h>
@@ -45,63 +44,48 @@ void Listener::showState(int hand)
 }
 
 
-// onInit
 void Listener::onInit( const Leap::Controller& controller )
 {
 }
 
-// onConnect
 void Listener::onConnect( const Leap::Controller& controller )
 {
-	// Enable Background App
 	controller.setPolicyFlags(Leap::Controller::POLICY_BACKGROUND_FRAMES); 
-
 	
-	// Enable detect Gesture
 	controller.enableGesture( Leap::Gesture::TYPE_SWIPE );
 	controller.enableGesture( Leap::Gesture::TYPE_SCREEN_TAP );
 	controller.enableGesture( Leap::Gesture::TYPE_CIRCLE);
 	controller.enableGesture( Leap::Gesture::TYPE_KEY_TAP);
 }
 
-// onDisconnect
 void Listener::onDisconnect( const Leap::Controller& controller )
 {
 }
 
-// onFrame
 void Listener::onFrame( const Leap::Controller& controller )
 {
 	
-		// Get Calibrated Screen List from Controller
 	const Leap::ScreenList screenList = controller.locatedScreens();
 	
-	// Get First Calibrated Screen from Screen List
 	if( screenList.isEmpty() ){
 		std::cout << "Calibrated Screen Not Found" << std::endl;
 		return;
 	}
 	screen = screenList[0];
 
-	// Get Frame from Controller
 	frame = controller.frame();
 
-	// Get Hand List from Frame
 	handList = frame.hands();
 
-	// Get PointableList from First Hand List
 	pointableList = handList[0].pointables();
 
-	// Get First Pointable from Pointable List
 	
 	if (handList[0].isLeft()){
 		if (!pointableList.isEmpty()){
 			const Leap::Pointable pointable = pointableList[0];
 
-			// Get Normalize Screen Intersect Position (Intersection of Screen and Destination of Pointable)
 			const Leap::Vector intersect = screen.intersect(pointable, true, 1.0f);
 			if (intersect.isValid()){
-				// Set Position of Mouse Cursor
 				unsigned int x = static_cast<unsigned int>(screen.widthPixels() * intersect.x);
 				unsigned int y = static_cast<unsigned int>(screen.heightPixels() * (1.0f - intersect.y));
 
@@ -110,16 +94,13 @@ void Listener::onFrame( const Leap::Controller& controller )
 		}
 	}
 
-	// Get GestureList from Frame
 	const Leap::GestureList gestureList = frame.gestures();
 	
-	// Get Gesture from GestureList
 	for( int count = 0; count < gestureList.count(); ++count ){
 		Leap::Gesture gesture = gestureList[count];
-		// Switch Gesture Type
+		
 		switch( gesture.type() ){
 
-			// Case of Swipe Gesture
 			case Leap::Gesture::TYPE_SWIPE:
 			{
 				Leap::SwipeGesture swipeGesture = gesture;
@@ -146,9 +127,7 @@ void Listener::onFrame( const Leap::Controller& controller )
 					}
 				}
 
-				// Vertical Swipe
 				else{
-					// Up Swipe
 					if( swipeGesture.direction().y > 0.0f ){
 						if( swipeGesture.state() == Leap::Gesture::STATE_START ){
 							std::cout << "↑" << std::endl;
@@ -157,7 +136,6 @@ void Listener::onFrame( const Leap::Controller& controller )
 						}
 					}
 
-					// Down Swipe
 					else{
 						if( swipeGesture.state() == Leap::Gesture::STATE_START ){
 							std::cout << "↓" << std::endl;
@@ -172,6 +150,7 @@ void Listener::onFrame( const Leap::Controller& controller )
 			case Leap::Gesture::TYPE_CIRCLE:
 			{	
 				CIRCLE++;
+				//実験用。シチュエーションが思い浮かばず断念。まず取れ方が不安定。多少精査しないとこのままでは使えない。
 				//std::cout << "CIRCLE" << CIRCLE << std::endl;
 				break;
 			}
@@ -216,18 +195,14 @@ void Listener::onExit( const Leap::Controller& controller )
 // Main
 int _tmain(int argc, _TCHAR* argv[])
 {
-	// Create a listener and controller
 	Listener listener;
 	Leap::Controller controller;
 
-	// Have the listener receive events from the controller
 	controller.addListener(listener);
 
-	// Keep this process running until Enter is pressed
 	std::cout << "Press Enter to quit..." << std::endl;
 	std::cin.get();
 
-	// Remove the listener when done
 	controller.removeListener(listener);
 
 	return 0;
